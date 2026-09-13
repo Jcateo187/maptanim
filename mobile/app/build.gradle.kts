eb5f85830cab6a0f1e6a1b476e6bd7cf1653eb15
Jcateo187 <jcateo187@gmail.com>
2026-09-13 22:00:02 +0800

    chore(mobile): bump versionCode to 2 and versionName to 1.2.5

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.maptanim.app"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
